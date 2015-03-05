8b3c48b1665f67c347a471e82e8b2f2917cfa315
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-05 07:11:38 -0800

    Remove duplicated methods now provided via common module. Do not require stack name within payload.

--- lib/jackal-stacks/wrecker.rb
+++ lib/jackal-stacks/wrecker.rb
@@ -5,6 +5,8 @@ module Jackal
     # Stack destroyer
     class Wrecker < Callback
 
+      include StackCommon
+
       # Setup callback
       def setup(*_)
         stacks_api
@@ -16,21 +18,7 @@ module Jackal
       # @return [Truthy, Falsey]
       def valid?(message)
         super do |payload|
-          payload.get(:data, :stacks, :wrecker) &&
-            payload.get(:data, :stacks, :name)
-        end
-      end
-
-      # @return [Miasma::Models::Orchestration]
-      def stacks_api
-        memoize(:stacks_api, :direct) do
-          Miasma.api_for(
-            config.fetch(
-              :orchestration_api, Smash.new
-            ).merge(
-              :type => :orchestration
-            )
-          )
+          payload.get(:data, :stacks, :wrecker)
         end
       end
 
@@ -39,14 +27,15 @@ module Jackal
       # @param message [Carnivore::Message]
       def execute(message)
         failure_wrap(message) do |payload|
-          stack = stacks_api.stacks.get(payload.get(:data, :stacks, :name))
+          s_name = stack_name(payload)
+          stack = stacks_api.stacks.get(s_name)
           if(stack)
-            info "Stack currently exists. Destroying. [#{stack}]"
+            info "Stack currently exists. Destroying. [#{stack.name}]"
             stack.destroy
             payload.set(:data, :stacks, :destroyed, true)
             job_completed(:stacks, payload, message)
           else
-            error "Failed to locate requested stack for destruction [#{payload.get(:data, :stacks, :name)}]"
+            error "Failed to locate requested stack for destruction [#{s_name}]"
             failed(payload, message, 'Requested stack does not exist')
           end
         end
