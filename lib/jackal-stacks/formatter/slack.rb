de8b5303ceab48e598ef8501722943d704eade88
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-06 12:37:47 -0800

    Update how the slack formatter interacts with payload. Add more useful information to message.

--- lib/jackal-stacks/formatter/slack.rb
+++ lib/jackal-stacks/formatter/slack.rb
@@ -17,13 +17,22 @@ module Jackal
         #
         # @param payload [Smash]
         def format(payload)
-          unless((notify = payload.get(:data, :stacks, {}).keys & NOTIFY_ON).empty?)
-            msgs = payload.fetch(:data, :slack, :messages, [])
-            msgs << Smash.new(
-              :description => "Stacks result: #{notify.first}",
-              :message => "Stack has been #{notify.first} [name: #{payload.get(:data, :stacks, :name)}]",
-              :color => :good
-            )
+          if(payload.get(:data, :stacks))
+            unless((notify = payload.fetch(:data, :stacks, {}).keys & NOTIFY_ON).empty?)
+              msgs = payload.fetch(:data, :slack, :messages, [])
+              msgs << Smash.new(
+                :description => "Stacks result: #{notify.first}",
+                :message => [
+                  "Stack has been #{notify.first} [name: #{payload.get(:data, :stacks, :name)}]",
+                  "* Template: #{payload.get(:data, :stacks, :template)}",
+                  "* Repository: #{payload.get(:data, :code_fetcher, :info, :owner)}/#{payload.get(:data, :code_fetcher, :info, :name)}",
+                  "* Reference: #{payload.get(:data, :code_fetcher, :info, :reference)}",
+                  "* SHA: #{payload.get(:data, :code_fetcher, :info, :commit_sha)}"
+                ].join("\n"),
+                :color => :good
+              )
+              payload.set(:data, :slack, :messages, msgs)
+            end
           end
         end
 
