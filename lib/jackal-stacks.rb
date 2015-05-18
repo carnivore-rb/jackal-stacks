023470256a00b5e93b50d2cf2dfaf6a385f1abdf
Chris Roberts <code@chrisroberts.org>
2015-05-18 10:59:56 -0700

    Add service registration information

--- lib/jackal-stacks.rb
+++ lib/jackal-stacks.rb
@@ -11,3 +11,36 @@ end
 
 require 'jackal-stacks/formatter'
 require 'jackal-stacks/version'
+
+Jackal.service(
+  :stacks,
+  :description => 'Manage stacks',
+  :configuration => {
+    :orchestration__bucket_name => {
+      :description => 'Name of remote bucket used for infrastructure storage'
+    },
+    :orchestration__api__credentials => {
+      :description => 'Remove credentials for API access',
+      :type => :hash
+    },
+    :default_template_path => {
+      :description => 'Relative path from repository root to default template'
+    },
+    :parameter_overrides => {
+      :description => 'Stack parameter value overrides',
+      :type => :hash
+    },
+    :init__chef__validator => {
+      :description => 'Auto generate Chef validator PEM',
+      :type => :boolean
+    },
+    :init__chef__encrypted_secret => {
+      :description => 'Auto generate Chef encrypted data bag secret',
+      :type => :boolean
+    },
+    :init__stable => {
+      :description => 'Auto create stable repository asset',
+      :type => :boolean
+    }
+  }
+)
